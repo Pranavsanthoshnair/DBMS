83983347af39a3c8e2b6668320cc78be5513d179
Pranav S Nair <pranavsnair2028@cs.sjcetpalai.ac.in>
2026-03-02 14:15:02 +0530

    Refactor SQL queries and clean up formatting

--- Student_Marks.sql
+++ Student_Marks.sql
@@ -1,5 +1,3 @@
--- Create Table
-
 CREATE TABLE Student
 (
 Roll INT PRIMARY KEY,
@@ -8,10 +6,6 @@ Physics INT,
 Chemistry INT,
 Mathematics INT
 );
-
-
--- Insert Values
-
 INSERT INTO Student VALUES (1,'Arun',20,18,40);
 INSERT INTO Student VALUES (2,'Meera',15,12,35);
 INSERT INTO Student VALUES (3,'Rahul',10,14,30);
@@ -22,57 +16,26 @@ INSERT INTO Student VALUES (7,'Ravi',8,15,20);
 INSERT INTO Student VALUES (8,'Divya',21,19,44);
 INSERT INTO Student VALUES (9,'Kiran',16,13,33);
 INSERT INTO Student VALUES (10,'Neha',12,12,25);
-
-
--- Class Average Physics
-
+SELECT * FROM STUDENT;
 SELECT AVG(Physics) AS average_physics
 FROM Student;
-
-
--- Highest Marks Mathematics
-
 SELECT MAX(Mathematics) AS highest_marks_maths
 FROM Student;
-
-
--- Lowest Marks Chemistry
-
 SELECT MIN(Chemistry) AS lowest_marks_chemistry
 FROM Student;
-
-
--- Students Passed Physics
-
 SELECT COUNT(*) AS pass_physics
 FROM Student
 WHERE Physics >= 12;
-
-
--- Students Passed All Subjects
-
 SELECT *
 FROM Student
 WHERE Physics>=12
 AND Chemistry>=12
 AND Mathematics>=25;
-
-
--- Total Marks of Each Student
-
 SELECT Roll,Name,
 Physics+Chemistry+Mathematics AS totalmark
 FROM Student;
-
-
--- Class Average Total Marks
-
 SELECT AVG(Physics+Chemistry+Mathematics) AS class_average
 FROM Student;
-
-
--- Total Students Passed
-
 SELECT COUNT(*) AS total_pass
 FROM Student
 WHERE Physics>=12
