4a716ad130bf6f04f5d12488ec872704e12bc02f
Pranav S Nair <pranavsnair2028@cs.sjcetpalai.ac.in>
2026-03-09 14:40:56 +0530

    Update Customer_Orders.sql

--- Customer_Orders.sql
+++ Customer_Orders.sql
@@ -27,35 +27,35 @@ CustID INT,
 OrderID INT
 );
 
-INSERT INTO Items VALUES
-(1,'Samsung Galaxy S4','Mobile',5200,10),
-(2,'iPhone 13','Mobile',7000,5),
-(3,'Dell Laptop','Laptop',4500,7),
-(4,'HP Laptop','Laptop',4800,6),
-(5,'Sony Headphones','Accessories',800,15),
-(6,'Logitech Mouse','Accessories',300,20);
+INSERT INTO Items VALUES (1,'Samsung Galaxy S4','Mobile',5200,10);
+INSERT INTO Items VALUES (2,'iPhone 13','Mobile',7000,5);
+INSERT INTO Items VALUES (3,'Dell Laptop','Laptop',4500,7);
+INSERT INTO Items VALUES (4,'HP Laptop','Laptop',4800,6);
+INSERT INTO Items VALUES (5,'Sony Headphones','Accessories',800,15);
+INSERT INTO Items VALUES (6,'Logitech Mouse','Accessories',300,20);
+SELECT * FROM Items;
 
-INSERT INTO Customers VALUES
-(1,'John','Delhi','Delhi'),
-(2,'Mickey','Mumbai','Maharashtra'),
-(3,'James','Chennai','Tamil Nadu'),
-(4,'Arun','Kochi','Kerala'),
-(5,'David','Pune','Maharashtra');
+INSERT INTO Customers VALUES (1,'John','Delhi','Delhi');
+INSERT INTO Customers VALUES (2,'Mickey','Mumbai','Maharashtra');
+INSERT INTO Customers VALUES (3,'James','Chennai','Tamil Nadu');
+INSERT INTO Customers VALUES (4,'Arun','Kochi','Kerala');
+INSERT INTO Customers VALUES (5,'David','Pune','Maharashtra');
+SELECT * FROM Customers;
 
-INSERT INTO Orders VALUES
-(101,1,1,2,'2013-01-10'),
-(102,2,3,1,'2013-02-15'),
-(103,2,1,1,'2013-03-20'),
-(104,3,2,1,'2012-12-25'),
-(105,4,5,3,'2014-01-11'),
-(106,2,6,2,'2015-02-05'),
-(107,2,4,1,'2016-04-18');
+INSERT INTO Orders VALUES (101,1,1,2,DATE'2013-01-10');
+INSERT INTO Orders VALUES (102,2,3,1,DATE'2013-02-15');
+INSERT INTO Orders VALUES (103,2,1,1,DATE'2013-03-20');
+INSERT INTO Orders VALUES (104,3,2,1,DATE'2012-12-25');
+INSERT INTO Orders VALUES (105,4,5,3,DATE'2014-01-11');
+INSERT INTO Orders VALUES (106,2,6,2,DATE'2015-02-05');
+INSERT INTO Orders VALUES (107,2,4,1,DATE'2016-04-18');
+SELECT * FROM Orders;
 
-INSERT INTO Delivery VALUES
-(1,1,101),
-(2,2,102),
-(3,3,104),
-(4,4,105);
+INSERT INTO Delivery VALUES (1,1,101);
+INSERT INTO Delivery VALUES (2,2,102);
+INSERT INTO Delivery VALUES (3,3,104);
+INSERT INTO Delivery VALUES (4,4,105);
+SELECT * FROM Delivery;
 
 SELECT State,COUNT(*) FROM Customers GROUP BY State;
 
@@ -104,7 +104,7 @@ FROM Customers
 WHERE CustID IN(
 SELECT CustID
 FROM Orders
-WHERE OrderDate>'2013-01-01'
+WHERE OrderDate>DATE'2013-01-01'
 )
 AND CustID NOT IN(
 SELECT CustID FROM Delivery
